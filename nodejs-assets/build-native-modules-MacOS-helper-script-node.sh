a915196c65f6a4ceefd6f223b3ec57f7227c6248
Dushmantha Tharaka <dushmanthatharaka@Dushmanthas-MacBook-Pro.local>
2019-06-24 13:18:07 +1200

    MOB-54: remove cached file

--- nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-      # Helper script for Gradle to call node on macOS in case it is not found
-      export PATH=$PATH:/usr/local/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/dushmanthatharaka/Documents/totara/totara-mobile-app/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/dushmanthatharaka/Documents/totara/totara-mobile-app/node_modules/.bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-      node $@
-    
