b966d486570267660367beeb944a1b0a14af56ca
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2019-09-12 16:35:28 +1200

    MOB-164 Added testcases for SitUrlHook

---
+++ src/totara/auth/manual/__tests__/SiteUrlHook-test.js
@@ -0,0 +1,117 @@
+/**
+ * This file is part of Totara Mobile
+ *
+ * Copyright (C) 2019 onwards Totara Learning Solutions LTD
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Jun Yamog <jun.yamog@totaralearning.com
+ */
+
+import { useSiteUrl } from "../SiteUrlHook";
+import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-test-renderer";
+
+describe("SiteUrlHook", () => {
+
+  it("should be an error if blank address", () => {
+    const { result } = renderHook(({siteUrl, onSiteUrlSuccess}) => useSiteUrl({siteUrl: siteUrl, onSiteUrlSuccess: onSiteUrlSuccess}), {initialProps: {siteUrl: undefined}});
+    
+    act(() => {
+      result.current.onSubmit();
+    });
+    
+    expect(result.current.siteUrlState).toMatchObject({
+      inputSiteUrlStatus: "error"
+    });
+  });
+
+  it("should be an error for invalid address", () => {
+    const { result } = renderHook(({siteUrl, onSiteUrlSuccess}) => useSiteUrl({siteUrl: siteUrl, onSiteUrlSuccess: onSiteUrlSuccess}), {initialProps: {siteUrl: "dafl"}});
+    
+    act(() => {
+      result.current.onSubmit();
+    });
+    
+    expect(result.current.siteUrlState).toMatchObject({
+      inputSiteUrlStatus: "error"
+    });
+  });
+
+  it("should be a success for a valid address", () => {
+    const successMock = jest.fn((data)=> {
+      expect(data).toBe("https://abc.com");
+    });
+    const { result } = renderHook(({siteUrl, onSiteUrlSuccess}) => useSiteUrl({siteUrl: siteUrl, onSiteUrlSuccess: onSiteUrlSuccess}), {initialProps: {siteUrl: "https://abc.com", onSiteUrlSuccess: successMock}});
+    
+    act(() => {
+      result.current.onSubmit();
+    });
+    
+    expect(result.current.siteUrlState).toMatchObject({
+      inputSiteUrlStatus: "success",
+      inputSiteUrl: "https://abc.com"
+    });
+  });
+
+  it("should use the initial address if it is present with a protocol", () => {
+    const successMock = jest.fn((data)=> {
+      expect(data).toBe("http://abc.com");
+    });
+    const { result } = renderHook(({siteUrl, onSiteUrlSuccess}) => useSiteUrl({siteUrl: siteUrl, onSiteUrlSuccess: onSiteUrlSuccess}), {initialProps: {siteUrl: "http://abc.com", onSiteUrlSuccess: successMock}});
+    
+    expect(result.current.siteUrlState).toMatchObject({
+      inputSiteUrl: "http://abc.com"
+    });
+  });
+
+  it("should use the initial address as it is without a protocol", () => {
+    const successMock = jest.fn((data)=> {
+      expect(data).toBe("https://abc.com");
+    });
+    const { result } = renderHook(({siteUrl, onSiteUrlSuccess}) => useSiteUrl({siteUrl: siteUrl, onSiteUrlSuccess: onSiteUrlSuccess}), {initialProps: {siteUrl: "abc.com", onSiteUrlSuccess: successMock}});
+    
+    expect(result.current.siteUrlState).toMatchObject({
+      inputSiteUrl: "abc.com"
+    });
+  });
+
+  it("should add the protocol when it not present and address is valid", () => {
+    const successMock = jest.fn((data)=> {
+      expect(data).toBe("https://abc.com");
+    });
+    const { result } = renderHook(({siteUrl, onSiteUrlSuccess}) => useSiteUrl({siteUrl: siteUrl, onSiteUrlSuccess: onSiteUrlSuccess}), {initialProps: {siteUrl: "abc.com", onSiteUrlSuccess: successMock}});
+    
+    act(() => {
+      result.current.onSubmit();
+    });
+
+    expect(result.current.siteUrlState).toMatchObject({
+      inputSiteUrl: "https://abc.com"
+    });
+  });
+
+  it("should change address while changing", () => {
+    const { result } = renderHook(({siteUrl, onSiteUrlSuccess}) => useSiteUrl({siteUrl: siteUrl, onSiteUrlSuccess: onSiteUrlSuccess}), {initialProps: {siteUrl: undefined}});
+    
+    act(() => {
+      result.current.onChangeInputSiteUrl("pqr.com");
+    });
+    
+    expect(result.current.siteUrlState).toMatchObject({
+      inputSiteUrl: "pqr.com"
+    });
+  });
+
+});
