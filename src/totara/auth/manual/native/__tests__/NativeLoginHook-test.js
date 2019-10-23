6fd908daddb1b3f1b3cce48f3a96750bdc6c16eb
Tharaka Dushmantha <tharaka.dushmantha@totaralearning.com>
2019-10-24 09:30:15 +1300

    MOB-172 auth: removed useState and implement reducer

--- src/totara/auth/manual/native/__tests__/NativeLoginHook-test.js
+++ src/totara/auth/manual/native/__tests__/NativeLoginHook-test.js
@@ -19,9 +19,115 @@
  * @author Tharaka Dushmantha <tharaka.dushmantha@totaralearning.com>
  */
 
-import { useWebviewLogin } from "../WebviewLoginHook";
-import { renderHook } from "@testing-library/react-hooks";
-import { act } from "react-test-renderer";
+import { renderHook, act } from "@testing-library/react-hooks";
+import {
+  useNativeLogin,
+  nativeReducer,
+  fetchLoginSecret,
+  fetchLogin
+} from "../NativeLoginHook";
 
+const onBack = jest.fn();
+const onSetupSecretSuccess = jest.fn();
 
+const { result } = renderHook(props => useNativeLogin(props), {
+    initialProps: {
+      siteUrl: "http://mobiledemo.wlg.totaralms.com",
+      onSetupSecretSuccess: { onSetupSecretSuccess },
+      onBack: { onBack }
+    }
+});
 
+describe("useNativeLogin", () => {
+  
+  it("both empty 'username' and 'password'", () => {
+    act(() => {
+      result.current.inputUsernameWithShowError(undefined);
+      result.current.inputPasswordWithShowError(undefined);
+      result.current.onClickEnter();
+    });
+
+    expect(result.current.nativeLoginState.inputUsernameStatus).toBe("error");
+    expect(result.current.nativeLoginState.inputPasswordStatus).toBe("error");
+  });
+
+  it("valid 'username' and empty 'password'", () => {
+    act(() => {
+      result.current.inputUsernameWithShowError("username");
+      result.current.inputPasswordWithShowError(undefined);
+      result.current.onClickEnter();
+    });
+
+    expect(result.current.nativeLoginState.inputUsernameStatus).toBe("error");
+    expect(result.current.nativeLoginState.inputPasswordStatus).toBe("error");
+  });
+
+  it("empty 'username' and valid 'password'", () => {
+    act(() => {
+      result.current.inputUsernameWithShowError(undefined);
+      result.current.inputPasswordWithShowError("password");
+      result.current.onClickEnter();
+    });
+
+    expect(result.current.nativeLoginState.inputUsernameStatus).toBe("error");
+    expect(result.current.nativeLoginState.inputPasswordStatus).toBe("error");
+  });
+
+  it("both valid 'username' and 'password'", () => {
+    act(() => {
+      result.current.inputUsernameWithShowError("username");
+      result.current.inputPasswordWithShowError("password");
+    });
+
+    expect(result.current.nativeLoginState.inputUsername).toBe("username");
+    expect(result.current.nativeLoginState.inputPassword).toBe("password");
+  });
+});
+
+// describe("Native login reducer", () => {
+//     it("should put flowStep into when received setup-secret", () => {
+//       const currentState = {
+//         setupSecret: undefined
+//       };
+//       const action = {
+//         type: "setupSecret",
+//         payload: "setupsecret"
+//       };
+//       const newState = nativeReducer(currentState, action);
+//       expect(newState.setupSecret).toBe("setupsecret");
+//     });
+// });
+describe("fetchData", () => {
+it("should call onLoginFailure if the response has an error", async () => {
+    expect.assertions(2);
+    const dispatch = jest.fn(({type, payload}) => {
+      expect(type).toBe("setupsecret");
+      expect(payload).toBe(
+        "ZCLFQXKQKzuVjklcpNeyRw4LvMudSQ"
+      )
+    });
+    await fetchLogin(mockFetchLoginSetup)( dispatch, "loginsecret", "username", "password");
+  });
+
+//   it("should call onLoginFailure if the response has an error", async () => {
+//     expect.assertions(2);
+//     const dispatch = jest.fn(({type, payload}) => {
+//       expect(type).toBe("setupsecret");
+//       expect(payload).toBe(
+//         "ZCLFQXKQKzuVjklcpNeyRw4LvMudSQ"
+//       )
+//     });
+//     await fetchLogin(mockFetchLoginSetup)( dispatch, "loginsecret", "username", "password");
+//   });
+});
+
+  const mockFetchLoginSetup = () => {
+    return Promise.resolve({
+      status: 200,
+      json: () => ({
+       "data": {
+        "setupsecret": "ZCLFQXKQKzuVjklcpNeyRw4LvMudSQ"
+        }
+      })
+    });
+  };
