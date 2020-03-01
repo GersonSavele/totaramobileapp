9a00d4873ccab00c713212c1c66078b6dfd04b3b
Rodrigo Mathias <rodrigo.mathias@totaralearning.com>
2020-03-02 10:35:46 +1300

    MOB-411 Added packageProcessor (former ScormProcessor)

---
+++ src/totara/activities/scorm/offline/packageProcessor.ts
@@ -0,0 +1,112 @@
+/**
+ * This file is part of Totara Mobile
+ *
+ * Copyright (C) 2019 onwards Totara Learning Solutions LTD
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author: Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
+ */
+
+import * as RNFS from 'react-native-fs';
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+
+export const getScormPackageData = (path: string, packageName: string) => {
+    return new Promise(function (resolve, reject) {
+        const manifestFilePath = `${path}/${packageName}/imsmanifest.xml`;
+        RNFS.readFile(manifestFilePath)
+            .then(xmlcontent => {
+                const xmlData = new dom().parseFromString(xmlcontent);
+                const scosList = getScosDataForPackage(xmlData);
+                const defaultSco = getInitialScormLoadData(xmlData);
+                resolve({scos: scosList, default: defaultSco});
+            }).catch((error: any)=>{
+                reject(error);
+        });
+    });
+};
+
+const getScosDataForPackage = (manifestDom: any) => {
+    const resultOrganisations = xpath.evaluate(
+        // "//*[local-name(.)='organizations' and namespace-uri(.)='http://www.imsproject.org/xsd/imscp_rootv1p1p2']/*[local-name()='organization']/*[local-name()='item']/@identifier", // xpathExpression
+        "//*[local-name(.)='organizations']/*[local-name()='organization']/@identifier",
+        manifestDom, // contextNode
+        null, // namespaceResolver
+        xpath.XPathResult.ANY_TYPE, // resultType
+        null, // result
+    );
+
+    let organizationNode = resultOrganisations.iterateNext();
+    let scos = [];
+    while (organizationNode) {
+        const itemResult = xpath.evaluate("//*[local-name(.)='item']/@identifier", organizationNode, null, xpath.XPathResult.ANY_TYPE, null);
+        let itemNode = itemResult.iterateNext();
+        while (itemNode) {
+            const valLaunchUrl = getDefaultScoLaunchUrl(manifestDom, itemNode.nodeValue);
+            const sco = {scoId: itemNode.nodeValue, organizationId:  organizationNode.nodeValue, launchSrc: valLaunchUrl};
+            scos.push(sco);
+            itemNode = itemResult.iterateNext();
+        }
+        organizationNode = resultOrganisations.iterateNext();
+    }
+    return scos;
+};
+
+const getInitialScormLoadData = (manifestDom: any) => {
+    const defaultOrgizationsNode = xpath.select("//*[local-name(.)='organizations']/*[local-name(.)='organization']/@identifier", manifestDom);
+    let defaultOrgizationId = null;
+    if (defaultOrgizationsNode && defaultOrgizationsNode.length > 0) {
+        if (defaultOrgizationsNode.length === 1) {
+            defaultOrgizationId = defaultOrgizationsNode[0].nodeValue;
+        } else {
+            const defaultOrgNode = xpath.select("//*[local-name(.)='organizations']/@default", manifestDom);
+            if (defaultOrgNode.length === 1 && defaultOrgNode[0].nodeValue) {
+                const tempDefaultOrg = defaultOrgNode[0].nodeValue;
+                for (let i = 0; i < defaultOrgizationsNode.length; i++) {
+                    if (defaultOrgizationsNode[i].nodeValue === tempDefaultOrg) {
+                        defaultOrgizationId = tempDefaultOrg;
+                        break;
+                    }
+                }
+            }
+            if (!defaultOrgizationId) {
+                defaultOrgizationId = defaultOrgizationsNode[0].nodeValue;
+            }
+        }
+    }
+    let defaultScoId = null;
+    let defaultLaunchSrc = null;
+    if (defaultOrgizationId !== null) {
+        const firstScoOnDefaultOrgNode = xpath.select("//*[local-name(.)='organizations']/*[local-name(.)='organization' and @identifier='"+defaultOrgizationId+"']/*[local-name(.)='item'][1]", manifestDom);
+        if (firstScoOnDefaultOrgNode && firstScoOnDefaultOrgNode.length === 1) {
+            defaultScoId = firstScoOnDefaultOrgNode[0].getAttribute('identifier');
+            defaultLaunchSrc = getDefaultScoLaunchUrl(manifestDom, defaultScoId);
+        }
+    }
+    return {scoId: defaultScoId, organizationId: defaultOrgizationId, launchSrc: defaultLaunchSrc};
+};
+
+const getDefaultScoLaunchUrl = (manifestDom: any, scoId: string) => {
+    const scoNode = xpath.select("//*[local-name(.)='item' and @identifier ='"+scoId+"']", manifestDom);
+    if (scoNode.length === 1) {
+        const resouceId = scoNode[0].getAttribute('identifierref');
+        const queryString = scoNode[0].getAttribute('parameters');
+        const resourceNode = xpath.select("//*[local-name(.)='resources']/*[local-name(.)='resource' and @identifier ='"+resouceId+"']", manifestDom);
+        if (resourceNode && resourceNode.length === 1 && resourceNode[0].getAttribute('href') ) {
+            return `./complexscorm/${resourceNode[0].getAttribute('href')}${queryString}`;
+        }
+    }
+    return null;
+};
