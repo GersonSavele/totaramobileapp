dee0c3629aa2715832def5e0d86d958f4a7a9862
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2020-03-04 10:29:46 +1300

    MOB-412 activity: build issue fixed after merge

--- android/app/src/main/java/com/totaramobileapp/MainApplication.java
+++ android/app/src/main/java/com/totaramobileapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.totaramobileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.futurepress.staticserver.FPStaticServerPackage;
 import com.rnziparchive.RNZipArchivePackage;
 import com.rnfs.RNFSPackage;
 import cx.evermeet.versioninfo.RNVersionInfoPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FPStaticServerPackage(),
             new RNZipArchivePackage(),
             new RNFSPackage(),
             new RNVersionInfoPackage(),
