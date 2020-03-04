028ddbd1f7c70baf47dd338deabbfce112b41c4c
Rodrigo Mathias <rodrigo.mathias@totaralearning.com>
2020-03-05 11:53:15 +1300

    MOB-413 adding back theme

--- src/totara/features/my-learning/NoCurrentLearning.js
+++ src/totara/features/my-learning/NoCurrentLearning.js
@@ -31,6 +31,8 @@ import {ScormActivity} from "@totara/types"
 
 const NoCurrentLearning = () => {
 
+  const [theme] = useContext(ThemeContext);
+
   return (
     <View style={styles.containerStyle}>
       <Image style={styles.imageContainer} source={require("@resources/images/no_current_learning/no_current_learning.png")} />
