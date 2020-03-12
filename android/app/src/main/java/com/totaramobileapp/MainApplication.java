32d08b18c0d4470fafc4c4bb299b88d55fce9284
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2020-03-12 17:02:55 +1300

    MOB-439 activity: scorm android crash fix

--- android/app/src/main/java/com/totaramobileapp/MainApplication.java
+++ android/app/src/main/java/com/totaramobileapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.totaramobileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.futurepress.staticserver.FPStaticServerPackage;
+import com.rnziparchive.RNZipArchivePackage;
 import com.rnfs.RNFSPackage;
 import cx.evermeet.versioninfo.RNVersionInfoPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -35,6 +37,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FPStaticServerPackage(),
+            new RNZipArchivePackage(),
             new RNFSPackage(),
             new RNVersionInfoPackage(),
             new AsyncStoragePackage(),
