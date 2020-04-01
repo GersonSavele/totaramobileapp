e068d11609a1deca5d44ca78c26003bcddf81f1f
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2020-04-01 18:50:07 +1300

    MOB-460 scorm: feedback images and LinkText component

---
+++ src/totara/components/LinkText.tsx
@@ -0,0 +1,41 @@
+/**
+ * This file is part of Totara Mobile
+ *
+ * Copyright (C) 2019 onwards Totara Learning Solutions LTD
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author: Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
+ */
+
+import React, { useContext } from "react";
+import { Text, TextStyle, TouchableOpacity } from "react-native";
+
+import { ThemeContext } from "@totara/theme";
+
+type Props = {
+  text?: string,
+  style?: TextStyle,
+  onPress?: (() => void)
+}
+
+const LinkText = ({ text, style, onPress, ...rest }: Props) => {
+
+  const [theme] = useContext(ThemeContext);
+  
+  return (<TouchableOpacity onPress={onPress}>
+    <Text style={[theme.textB1, {color: theme.colorLink}, style]} {...rest}>{text}</Text>
+  </TouchableOpacity>);
+};
+export default LinkText;
