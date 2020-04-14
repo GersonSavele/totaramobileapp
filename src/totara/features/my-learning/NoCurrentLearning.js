caf2a4bc8dfbcbefe3a1d587192c0dadd5b22499
Tharaka Dushmantha <tharaka.dushmantha@totaralearning.com>
2020-04-15 08:16:25 +1200

    MOB-489 auth: Bug fixed in 'Go to browser' button crashes app from current learning empty state

--- src/totara/features/my-learning/NoCurrentLearning.js
+++ src/totara/features/my-learning/NoCurrentLearning.js
@@ -37,7 +37,7 @@ const NoCurrentLearning = () => {
       <Text style={[theme.textH2, styles.description]}>{translate("my-learning.no_learning_message")}</Text>
       <AuthConsumer>
         {auth => (
-          <PrimaryButton onPress={() => { Linking.openURL(auth.appState.host); }} text={translate("additional-actions-modal.auth_model_go_to_browser")} icon="external-link-alt" style={{ alignSelf: "center" }} />
+          auth.authContextState.appState && auth.authContextState.appState.host && <PrimaryButton onPress={() => { Linking.openURL(auth.authContextState.appState.host) }} text={translate("additional-actions-modal.auth_model_go_to_browser")} icon="external-link-alt" style={{ alignSelf: "center" }} />
         )}
       </AuthConsumer>
     </View>
