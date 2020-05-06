6f85d44477fa5db221a2bc3562f1b65607ffab73
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2020-05-06 17:50:41 +1200

    MOB-500 scorm: android activity top-bar alignment fixes

--- src/totara/components/ResourceDownloader.tsx
+++ src/totara/components/ResourceDownloader.tsx
@@ -6,45 +6,81 @@ import { ThemeContext, gutter } from "@totara/theme";
 import { ResourceState } from "@totara/core/ResourceManager/Resource";
 
 type ResourceDownloaderProps = {
-    mode?: ResourceState
-    onPress?: () => void,
-    progress: number,
-    size?: number,
-    style?: ImageStyle
+  mode?: ResourceState;
+  onPress?: () => void;
+  progress: number;
+  size?: number;
+  style?: ImageStyle;
 };
 
 const DownloadIcon = {
-    solid: require("@resources/images/tabbar/downloads_solid.png"),
-    regular: require("@resources/images/tabbar/downloads_regular.png")
+  solid: require("@resources/images/tabbar/downloads_solid.png"),
+  regular: require("@resources/images/tabbar/downloads_regular.png"),
 };
 
-const RosourceDownloaderComponent = ({mode, progress, size}: ResourceDownloaderProps) => {
-    const [theme] = useContext(ThemeContext);
-    if (mode === ResourceState.Downloading) {
-        return (
-        <Progress.Circle progress={progress/100}
-            size={size}
-            unfilledColor={theme.colorNeutral4}
-            color={theme.colorLink}
-            thickness={2}
-            borderWidth={0}
-            indeterminate={false}
-            formatText={() =>  ""}
-            showsText={true}
-            textStyle={{fontSize: theme.textSmall.fontSize, lineHeight: theme.textSmall.fontSize, color: theme.textColorDark, fontWeight: "bold", textAlign: "center"}}
-            />);
-    } else {
-        const iconSource = mode === ResourceState.Completed ? DownloadIcon.solid : DownloadIcon.regular;
-        return <Image source={iconSource} style={[{tintColor: theme.colorLink, height: size}]} resizeMode='contain' />;
-    }  
-};
-
-const ResourceDownloader = ({mode, onPress, progress, size, style}: ResourceDownloaderProps) =>{
+const RosourceDownloaderComponent = ({
+  mode,
+  progress,
+  size,
+}: ResourceDownloaderProps) => {
+  const [theme] = useContext(ThemeContext);
+  if (mode === ResourceState.Downloading) {
     return (
-    <TouchableOpacity onPress={()=> {onPress && onPress()}} disabled={!onPress} style={[{padding: gutter}, style]} >
-        <RosourceDownloaderComponent mode={mode} size={size} progress={progress} />
-    </TouchableOpacity>
+      <Progress.Circle
+        progress={progress / 100}
+        size={size}
+        unfilledColor={theme.colorNeutral4}
+        color={theme.colorLink}
+        thickness={2}
+        borderWidth={0}
+        indeterminate={false}
+        formatText={() => ""}
+        showsText={true}
+        textStyle={{
+          fontSize: theme.textSmall.fontSize,
+          lineHeight: theme.textSmall.fontSize,
+          color: theme.textColorDark,
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      />
     );
+  } else {
+    const iconSource =
+      mode === ResourceState.Completed
+        ? DownloadIcon.solid
+        : DownloadIcon.regular;
+    return (
+      <Image
+        source={iconSource}
+        style={[{ tintColor: theme.colorLink, height: size }]}
+        resizeMode="contain"
+      />
+    );
+  }
+};
+
+const ResourceDownloader = ({
+  mode,
+  onPress,
+  progress,
+  size,
+  style,
+}: ResourceDownloaderProps) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        onPress && onPress();
+      }}
+      disabled={!onPress}
+      style={[{ padding: gutter, alignSelf: "center" }, style]}>
+      <RosourceDownloaderComponent
+        mode={mode}
+        size={size}
+        progress={progress}
+      />
+    </TouchableOpacity>
+  );
 };
 
 export default ResourceDownloader;
