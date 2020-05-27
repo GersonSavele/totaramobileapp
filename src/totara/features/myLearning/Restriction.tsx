b56348dacbb7df0ac04ec1328599122911984512
Tharaka Dushmantha <tharaka.dushmantha@totaralearning.com>
2020-05-27 12:18:18 +1200

    MOB-560: Implemeted Restriction Modal when learning item not compatible to mobile

---
+++ src/totara/features/myLearning/Restriction.tsx
@@ -0,0 +1,59 @@
+/**
+ * This file is part of Totara Enterprise.
+ *
+ * Copyright (C) 2020 onwards Totara Learning Solutions LTD
+ *
+ * Totara Enterprise is provided only to Totara Learning Solutions
+ * LTD’s customers and partners, pursuant to the terms and
+ * conditions of a separate agreement with Totara Learning
+ * Solutions LTD or its affiliate.
+ *
+ * If you do not have an agreement with Totara Learning Solutions
+ * LTD, you may not access, use, modify, or distribute this software.
+ * Please contact [sales@totaralearning.com] for more information.
+ */
+
+import React from "react";
+import { Linking } from "react-native";
+import { translate } from "@totara/locale";
+import { PrimaryButton, InfoModal, TertiaryButton } from "@totara/components";
+import { AuthConsumer } from "@totara/core";
+
+type Props = {
+  onClose: () => void;
+};
+
+const Restriction = ({ onClose }: Props) => {
+  return (
+    <InfoModal
+      transparent={true}
+      title={translate("my-learning.restriction_view.title")}
+      description={translate("my-learning.restriction_view.description")}
+      imageType="self_completion"
+      visible>
+      <AuthConsumer>
+        {(auth) =>
+          auth.authContextState.appState &&
+          auth.authContextState.appState.host && (
+            <PrimaryButton
+              onPress={() => {
+                Linking.openURL(auth.authContextState.appState!.host);
+              }}
+              text={translate(
+                "my-learning.restriction_view.primary_button_title"
+              )}
+              icon="external-link-alt"
+              style={{ alignSelf: "center" }}
+            />
+          )
+        }
+      </AuthConsumer>
+      <TertiaryButton
+        text={translate("my-learning.restriction_view.tertiary_button_title")}
+        onPress={onClose}
+      />
+    </InfoModal>
+  );
+};
+
+export default Restriction;
