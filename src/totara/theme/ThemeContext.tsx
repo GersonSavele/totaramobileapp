2f2ef1921cc162a12cced1461703df91b2a9c376
Rodrigo Mathias <rodrigo.mathias@totaralearning.com>
2020-07-08 12:12:04 +1200

    MOB-506: addLocale and changeLocale

--- src/totara/theme/ThemeContext.tsx
+++ src/totara/theme/ThemeContext.tsx
@@ -23,6 +23,9 @@ import React, {
 
 import { AppliedTheme, TotaraTheme, applyTheme } from "./Theme";
 import { AuthContext } from "@totara/core";
+// @ts-ignore
+// import * as pt from "@totara/locale/languages/pt.json";
+// import { addLocale, changeLocale } from "@totara/locale";
 
 type Props = {
   children: ReactNode;
@@ -40,6 +43,10 @@ const ThemeProvider = ({ children }: Props) => {
     appState && appState.siteInfo && appState.siteInfo.theme
       ? applyTheme(appState.siteInfo.theme)
       : TotaraTheme;
+
+  // debugger;
+  // addLocale("pt", pt);
+  // changeLocale("en");
 
   return (
     <ThemeContext.Provider value={useState(customerTheme)}>
