894b7082ad83ff22850a5e9332f6c215b93ab209
Rodrigo Mathias <rodrigo.mathias@totaralearning.com>
2020-11-03 09:35:40 -0300

    MOB-828: With restore/save promise hack

--- jest.config.js
+++ jest.config.js
@@ -15,8 +15,10 @@
 const { defaults: tsjPreset } = require("ts-jest/presets");
 
 module.exports = {
+  verbose: true,
   ...tsjPreset,
-  preset: "react-native",
+  // preset: "react-native",
+  "preset": "./jest-preset.js",
   transform: {
     ...tsjPreset.transform,
     "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
@@ -34,7 +36,7 @@ module.exports = {
     "@totara/(.*)": "<rootDir>/src/totara/$1",
     "@resources/(.*)": "<rootDir>/src/resources/$1"
   },
-  testPathIgnorePatterns: ["e2e", "src/totara/features/downloads", "src/totara/features/notifications"],
+  testPathIgnorePatterns: ["e2e", "src/totara/features/notifications"],
   setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js", "<rootDir>/jest.setup.js"],
   collectCoverageFrom: ["src/totara/**/*.{js,jsx,ts,tsx}"],
   setupFilesAfterEnv: ["<rootDir>setup-tests.js"]
