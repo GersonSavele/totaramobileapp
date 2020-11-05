42c7aebfa635f7aee562773bfe4524ee0f66c583
Rodrigo Mathias <rodrigo.mathias@totaralearning.com>
2020-11-04 22:32:33 -0300

    MOB-26: Resorting with lodash

--- src/totara/features/currentLearning/utils.ts
+++ src/totara/features/currentLearning/utils.ts
@@ -1,32 +1,28 @@
 import { includes } from "lodash";
 import { learningItemEnum } from "@totara/features/currentLearning/constants";
+import {
+  sortBy} from "lodash";
 
 const extractTargetId = (id) => {
   return includes(id, "_") ? id.split("_")[1] : id;
 };
 
 const sortByDueDateThenTypeThenFullName = (toBeSorted) => {
-  return toBeSorted.sort((a, b) => {
-    if (a.duedate != null && (b.duedate === null || a.duedate < b.duedate)) return -1;
-    if (a.duedate != null && (b.duedate === null || a.duedate > b.duedate)) return 1;
-    if (
-      a.itemtype === learningItemEnum.Certification &&
-      (b.itemtype === learningItemEnum.Program || b.itemtype === learningItemEnum.Course)
-    )
-      return -1;
-    if (a.itemtype === learningItemEnum.Program) {
-      if (b.itemtype === learningItemEnum.Course) return -1;
-      else if (b.itemtype === learningItemEnum.Certification) return 1;
+
+  const typesForSorting = {
+    [learningItemEnum.Certification]: 1,
+    [learningItemEnum.Program]: 2,
+    [learningItemEnum.Course]: 3,
+  }
+
+  const withItemTypeMapped = toBeSorted.map(x=>{
+    return {
+      ...x,
+      itemTypeMapped: typesForSorting[x.itemtype]
     }
-    if (
-      a.itemtype === learningItemEnum.Course &&
-      (b.itemtype === learningItemEnum.Certification || b.itemtype === learningItemEnum.Program)
-    )
-      return 1;
-    if (a.fullname < b.fullname) return -1;
-    if (a.fullname > b.fullname) return 1;
-    else return 0;
   });
+
+  return sortBy(withItemTypeMapped, ['duedate', 'itemTypeMapped', 'fullname']);
 };
 
 export { extractTargetId, sortByDueDateThenTypeThenFullName };
