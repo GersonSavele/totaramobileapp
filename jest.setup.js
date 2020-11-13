8e6ec834d96dd0524f0e06d397e095695e3ed6aa
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2020-11-13 14:45:03 +1300

    MOB-840 activities: added mock for react-native-orientation-locker

--- jest.setup.js
+++ jest.setup.js
@@ -165,12 +165,12 @@ jest.mock("react-native-fs", () => ({
   PicturesDirectoryPath: jest.fn()
 }));
 
-jest.mock('redux-persist', () => {
-  const real = jest.requireActual('redux-persist');
+jest.mock("redux-persist", () => {
+  const real = jest.requireActual("redux-persist");
   return {
     ...real,
-    persistReducer: jest
-      .fn()
-      .mockImplementation((config, reducers) => reducers),
+    persistReducer: jest.fn().mockImplementation((config, reducers) => reducers)
   };
 });
+
+jest.mock("react-native-orientation-locker", () => jest.fn());
