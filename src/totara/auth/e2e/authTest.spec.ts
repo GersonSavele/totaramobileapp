b1050ff04a618a7fc772b2adf649687c057ebf30
Tharaka Dushmantha <tharaka.dushmantha@totaralearning.com>
2020-11-27 12:22:45 +1300

    MOB-851: Updarted project file

--- src/totara/auth/e2e/authTest.spec.ts
+++ src/totara/auth/e2e/authTest.spec.ts
@@ -1,11 +1,9 @@
 import { by, device, element } from "detox";
 import { TEST_IDS } from "../../lib/constants";
 
-enum typeText {
-  organizationUrl = "mobile.demo.totara.software",
-  userName = "tharakad",
-  password = "Abcd123$"
-}
+const ORGANIZATION_URL = "mobile.demo.totara.software";
+const USER_NAME = "tharakad";
+const PASSWORD = "Abcd123$";
 
 describe("User authentication", () => {
   beforeEach(async () => {
@@ -15,10 +13,10 @@ describe("User authentication", () => {
 
   it("should have organization url input and native login", async () => {
     await element(by.id(TEST_IDS.SITE_URL_INPUT)).clearText();
-    await element(by.id(TEST_IDS.SITE_URL_INPUT)).typeText(typeText.organizationUrl);
+    await element(by.id(TEST_IDS.SITE_URL_INPUT)).typeText(ORGANIZATION_URL);
     await element(by.id(TEST_IDS.SUBMIT_URL)).tap();
-    await element(by.id(TEST_IDS.USER_INPUT)).typeText(typeText.userName);
-    await element(by.id(TEST_IDS.USER_PW)).typeText(typeText.password);
+    await element(by.id(TEST_IDS.USER_INPUT)).typeText(USER_NAME);
+    await element(by.id(TEST_IDS.USER_PW)).typeText(PASSWORD);
     await element(by.id(TEST_IDS.LOGIN)).tap();
   });
 });
--- src/totara/features/currentLearning/e2e/currentLearningTest.spec.ts
+++ src/totara/features/currentLearning/e2e/currentLearningTest.spec.ts
@@ -1,10 +1,23 @@
 import { by, device, element } from "detox";
 import { TEST_IDS, CL_TEST_IDS } from "../../../lib/constants";
 
+const ORGANIZATION_URL = "mobile.demo.totara.software";
+const USER_NAME = "tharakad";
+const PASSWORD = "Abcd123$";
+
 describe("Current learning test", () => {
   beforeEach(async () => {
     await device.reloadReactNative();
     await device.launchApp({ newInstance: true, permissions: { notifications: "YES" } });
+  });
+
+  it("should have organization url input and native login", async () => {
+    await element(by.id(TEST_IDS.SITE_URL_INPUT)).clearText();
+    await element(by.id(TEST_IDS.SITE_URL_INPUT)).typeText(ORGANIZATION_URL);
+    await element(by.id(TEST_IDS.SUBMIT_URL)).tap();
+    await element(by.id(TEST_IDS.USER_INPUT)).typeText(USER_NAME);
+    await element(by.id(TEST_IDS.USER_PW)).typeText(PASSWORD);
+    await element(by.id(TEST_IDS.LOGIN)).tap();
   });
 
   it("should have user landing on current learning and switch to the list view and current learning", async () => {
