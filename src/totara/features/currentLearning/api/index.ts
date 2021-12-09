2d3608af995c0dd10df665c708b0fea7dbbf61bf
Kamala Tennakoon <kamala.tennakoon@totaralearning.com>
2021-12-09 15:59:02 +1300

    MOB-1077 detox: set currentlearning query for ui testing

--- src/totara/features/currentLearning/api/index.ts
+++ src/totara/features/currentLearning/api/index.ts
@@ -14,26 +14,48 @@
  */
 
 import { gql } from "@apollo/client";
+import { config } from "@totara/lib";
 
-const query = gql`
-  query totara_mobile_current_learning {
-    currentLearning: totara_mobile_current_learning {
-      id
-      itemtype
-      itemcomponent
-      shortname
-      fullname
-      summary: description(format: PLAIN)
-      progress
-      urlView: url_view
-      duedate(format: ISO8601)
-      duedateState: duedate_state
-      native: mobile_coursecompat
-      imageSrc: mobile_image
-      summaryFormat: description_format
-      __typename
-    }
-  }
-`;
+const query = config.mobileApi.persistentQuery
+  ? gql`
+      query totara_mobile_current_learning {
+        currentLearning: totara_mobile_current_learning {
+          id
+          itemtype
+          itemcomponent
+          shortname
+          fullname
+          summary: description(format: PLAIN)
+          progress
+          urlView: url_view
+          duedate(format: ISO8601)
+          duedateState: duedate_state
+          native: mobile_coursecompat
+          imageSrc: mobile_image
+          summaryFormat: description_format
+          __typename
+        }
+      }
+    `
+  : gql`
+      query mobile_currentlearning_my_items {
+        currentLearning: mobile_currentlearning_my_items {
+          id
+          itemtype
+          itemcomponent
+          shortname
+          fullname
+          summary: description(format: PLAIN)
+          progress
+          urlView: url_view
+          duedate(format: ISO8601)
+          duedateState: duedate_state
+          native: mobile_coursecompat
+          imageSrc: mobile_image
+          summaryFormat: description_format
+          __typename
+        }
+      }
+    `;
 
 export default query;
