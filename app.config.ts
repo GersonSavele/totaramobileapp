963f144f264d798217457dd3b86a9ddaa7068119
Chet Davidson <chethana96@gmail.com>
2024-07-16 07:03:30 +1200

    Add expo config to fix failing iOS builds

---
+++ app.config.ts
@@ -0,0 +1,48 @@
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+
+export default (): ExpoConfig => ({
+  slug: 'TotaraMobileApp',
+  name: 'TotaraMobileApp',
+  version: '1.3.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'totara',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/images/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff'
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.totaralearning.TotaraMobileApp',
+    buildNumber: '14',
+    googleServicesFile: process.env.EAS_BUILD ? process.env.GOOGLE_SERVICES_FILE_IOS : './GoogleService-Info.plist'
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff'
+    },
+    package: 'com.totaralearning.TotaraMobileApp',
+    googleServicesFile: process.env.EAS_BUILD ? process.env.GOOGLE_SERVICES_FILE_ANDROID : './google-services.json'
+  },
+  plugins: [
+    '@react-native-firebase/app',
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          useFrameworks: 'static'
+        }
+      }
+    ]
+  ],
+  experiments: {},
+  extra: {
+    eas: {
+      projectId: '7a0f187a-4f59-4859-8d6e-728e5d65421f'
+    }
+  },
+  owner: 'abletech'
+});
