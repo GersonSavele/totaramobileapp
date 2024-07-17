addb7755c882279632bab03327b033cbf4161d17
Chet Davidson <chethana96@gmail.com>
2024-07-18 11:52:23 +1200

    Ensure translations are downloaded for build

--- scripts/eas/eas-build-pre-install.sh
+++ scripts/eas/eas-build-pre-install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Install cmake as required for the react-native-static-server library
 if [[ "$EAS_BUILD_PLATFORM" == "android" ]]; then
   apt-get install --yes cmake
 elif [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
   HOMEBREW_NO_AUTO_UPDATE=1 brew install cmake
 fi
+
+# Download locale translations for the build
+npm run translations
