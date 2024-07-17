fdce9215004c569ccd86a98055946080ad7f4be3
Chet Davidson <chethana96@gmail.com>
2024-07-18 11:15:56 +1200

    Update pre-install hook

--- scripts/eas/eas-build-pre-install.sh
+++ scripts/eas/eas-build-pre-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$EAS_BUILD_PLATFORM" == "android" ]]; then
-  echo "apt-get install --yes cmake"
+  apt-get install --yes cmake
 elif [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
-  echo "HOMEBREW_NO_AUTO_UPDATE=1 brew install cmake"
+  HOMEBREW_NO_AUTO_UPDATE=1 brew install cmake
 fi
